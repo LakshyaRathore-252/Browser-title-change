73ad15073085aa5fd5dafc56bdebfc8f305c8f9b
Lakshya Rathore <97387362+LakshyaRathore-252@users.noreply.github.com>
2023-02-22 18:11:23 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let docTitle = document.title;
+window.addEventListener("blur",() =>{
+  document.title = "Come Back ;(";
+
+});
+
+
+window.addEventListener("focus",() =>{
+  document.title = docTitle;
+
+});
+
